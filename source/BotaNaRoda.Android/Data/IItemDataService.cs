334c06388af638d9828a1eabe1040012ec056982
Lucas Espindola <espdlucas@gmail.com>
2015-06-04 12:09:17 -0300

    feat(client) json data service and tests for item

---
+++ source/BotaNaRoda.Android/Data/IItemDataService.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BotaNaRoda.Android.Entity;
+
+namespace BotaNaRoda.Android.Data
+{
+    public interface IItemDataService
+    {
+        IEnumerable<Item> GetAllItems();
+        void RefreshCache();
+        Item GetItem(string id);
+        void SaveItem(Item item);
+        void DeleteItem(Item item);
+    }
+}
---
+++ source/BotaNaRoda.Android/Data/ItemJsonService.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BotaNaRoda.Android.Entity;
+using Newtonsoft.Json;
+
+namespace BotaNaRoda.Android.Data
+{
+    public class ItemJsonService : IItemDataService
+    {
+        private readonly string _storagePath;
+        private readonly List<Item> _items = new List<Item>();
+ 
+        public ItemJsonService(string storagePath)
+        {
+            _storagePath = storagePath;
+
+            if (!Directory.Exists(_storagePath))
+                Directory.CreateDirectory(_storagePath);
+
+            RefreshCache();
+        }
+
+        public IEnumerable<Item> GetAllItems()
+        {
+            return _items;
+        }
+
+        public void RefreshCache()
+        {
+            _items.Clear();
+            var fileNames = Directory.GetFiles(_storagePath, "*.json");
+
+            foreach (var fileName in fileNames)
+            {
+                string itemString = File.ReadAllText(fileName);
+                Item item = JsonConvert.DeserializeObject<Item>(itemString);
+                _items.Add(item);
+            }
+        }
+
+        public Item GetItem(string id)
+        {
+            return _items.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public void SaveItem(Item item)
+        {
+            bool newItem = item.Id == null;
+            if (newItem)
+            {
+                item.Id = Guid.NewGuid().ToString();
+            }
+            item.PostDate = DateTime.UtcNow;
+
+            var itemString = JsonConvert.SerializeObject(item);
+            File.WriteAllText(GetFilename(item.Id), itemString);
+        
+            if(newItem)
+                _items.Add(item);
+        }
+
+        public void DeleteItem(Item item)
+        {
+            File.Delete(GetFilename(item.Id));
+            _items.Remove(item);
+        }
+
+        private string GetFilename(string id)
+        {
+            return Path.Combine(_storagePath, string.Format("item{0}.json", id));
+        }
+    }
+}
