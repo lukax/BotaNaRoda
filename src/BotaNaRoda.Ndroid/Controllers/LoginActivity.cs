691f2b9b64be1155467d0626db7b6522a3ce7537
Lucas Espindola <espdlucas@gmail.com>
2015-08-17 07:50:19 -0300

    feat(client) login activity

---
+++ src/BotaNaRoda.Ndroid/Controllers/LoginActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Content.PM;
+
+namespace BotaNaRoda.Ndroid
+{
+	[Activity (Label = "LoginActivity",
+		ConfigurationChanges = (ConfigChanges.Orientation | ConfigChanges.ScreenSize))]		
+	public class LoginActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Login);
+		}
+
+	}
+}
+
